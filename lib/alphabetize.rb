db740764d67e56f7df128dcccd4b772b2ca53400
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-09 22:34:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,9 @@
 def alphabetize(arr)
-  arr.sort_by do |str|
-    str.split("").map do |letter|
-      
+  arr.sort_by { |str|
+    str.split("").map { |letter|
+    ESPERANTO_ALPHABET.index(letter) } }
 end
+
 
 
 
